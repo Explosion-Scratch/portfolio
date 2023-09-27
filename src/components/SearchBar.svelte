e2d080fc74d2a4b1d6f6bfd8e743a865506e9bc7
Explosion Implosion <explosion.scratch@gmail.com>
2023-09-27 06:44:26 -0500

    Backup

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import Tag from "./Tag.svelte";
   import { strip } from "../utils";
   import Fuse from "fuse.js";
@@ -9,11 +9,21 @@
   let fuse = {
     search: () => totalProjects.map((i) => ({ item: i })),
   };
+  let placeholderInt;
+  let placeholder = "Search";
   onMount(() => {
     totalProjects = [...projects];
+    let placeholders = totalProjects.map((i) => `Search for "${i.title}""`);
     fuse = new Fuse(totalProjects, {
       keys: ["title", "description", "tags"],
     });
+    placeholderInt = setInterval(() => {
+      placeholder =
+        placeholders[Math.floor(Math.random() * placeholders.length)];
+    }, 1000);
+  });
+  onDestroy(() => {
+    clearInterval(placeholderInt);
   });
   $: tags = [
     ...new Set(
@@ -37,23 +47,46 @@
     console.log(fuse.search("spanish"));
     projects = fuse.search(q).map((i) => i.item);
   }
+
+  let tagsExpanded = false;
+  let inputFocused = false;
 </script>
 
 <div class="input_container">
   <div class="input">
     <div class="bg"></div>
     <input
-      placeholder="Type here"
+      on:focus="{() => (inputFocused = true)}"
+      on:blur="{() => (inputFocused = false)}"
+      placeholder="{inputFocused ? placeholder : 'Search...'}"
       bind:value="{query}"
       on:change="{handle}"
       on:keyup="{handle}"
       on:input="{handle}"
     />
   </div>
-  <div class="tags">
-    {#each tags as tag}
-      <Tag tag="{tag}" />
-    {/each}
+  <div class="tags_container" class:expanded="{tagsExpanded}">
+    <div class="tags" class:expanded="{tagsExpanded}">
+      {#each tags as tag}
+        <Tag tag="{tag}" />
+      {/each}
+    </div>
+    <button
+      class="icon pointer chevron"
+      on:click="{() => (tagsExpanded = !tagsExpanded)}"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        ><path
+          fill="currentColor"
+          d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z"
+        ></path></svg
+      >
+      {tagsExpanded ? "Collapse" : "Expand"}
+    </button>
   </div>
 </div>
 
@@ -62,9 +95,52 @@
 
   $secondary: adjust-hue(lighten($primary, 20), -10);
   $background: black;
-  .tags {
+  .icon {
+    cursor: pointer;
+    color: white;
+    opacity: 0.6;
+    font-size: 0;
+    cursor: pointer;
+    transition: opacity 0.3s ease, transform 0.4s ease;
+    &:hover {
+      opacity: 0.8;
+    }
+    &:active {
+      transform: scale(0.95);
+    }
+    svg {
+      transition: transform 0.5s ease;
+    }
+    background: transparent;
+  }
+  .tags_container {
+    display: flex;
+    &.expanded {
+      flex-direction: column;
+      .icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1em;
+        margin-top: 0.5em;
+        border: 2px dashed rgba($primary, 0.5);
+        color: rgba(lighten($primary, 45), 0.8);
+        svg {
+          transform: rotate(180deg);
+        }
+        &:hover {
+          border-style: solid;
+          border-radius: 0.3em;
+        }
+      }
+    }
+  }
+  .tags:not(.expanded) {
     overflow-x: scroll;
     white-space: nowrap;
+    $mask: linear-gradient(to right, black 80%, transparent);
+    -webkit-mask-image: $mask;
+    mask-image: $mask;
   }
   .input_container {
     width: 80vw;
